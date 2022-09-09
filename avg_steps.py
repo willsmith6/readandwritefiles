e0ac7b2042fbd56a6561c47959ed865f5e10a53e
Will <Will@test.com>
2022-09-08 23:26:33 -0500

    read and write

--- avg_steps.py
+++ avg_steps.py
@@ -1,29 +1,38 @@
 def main():
-    
+
     import csv
 
     infile = open('steps.csv','r')
     csvfile = csv.reader(infile, delimiter=',')
     outfile = open('avg_steps.csv','w')
-    counter = 1
+
+    counter = 1 
     step_total = 0
     month_cnt = 0
 
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
     next(csvfile)
+
+
     for record in csvfile: 
+
         if record[0] == str(counter):   
+
             step_cnt = int(record[1])
             step_total = step_cnt + step_total
             month_cnt += 1
+        
+        else:
+            month_avg = (step_total)/(month_cnt)
+            #outfile.write(months)
+            outfile.write(str(month_avg))
+            outfile.write('\n')
+            step_total = step_cnt + step_total
 
-        #outfile.write('Monthly Total: ')
-        #outfile.write(str(step_total))
-        #outfile.write(' ')
+            counter = record[0]
 
-        outfile.write('Monthly Average: ')
-        monthly_avg = (step_total)/(month_cnt)
-        outfile.write(str(monthly_avg))
+    outfile.close()
 
-        outfile.write('\n')
 
 main()
